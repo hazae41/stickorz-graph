ff2ae6ef3cd02565e91cf43ab7cef8d7b2167fcd
hazae41 <hazae41@gmail.com>
2021-12-21 21:33:21 +0100

    count

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,7 +4,15 @@ import {
   Transfer,
   Upvote
 } from "../generated/Bank/Bank"
-import { Sticker } from "../generated/schema"
+import { Count, Sticker } from "../generated/schema"
+
+function getCount(): Count {
+  const count = Count.load("1")
+  if (count) return count
+  const ncount = new Count("1")
+  ncount.count = 0
+  return ncount
+}
 
 export function handleApproval(event: Approval): void { }
 
@@ -24,6 +32,11 @@ export function handleMint(event: Mint): void {
   sticker.transfered_at = event.block.number
   if (!sticker.hash.startsWith("Qm"))
     return
+  if (!sticker.tags)
+    return
+  const count = getCount()
+  count.count = count.count + 1
+  count.save()
   sticker.save()
 }
 
